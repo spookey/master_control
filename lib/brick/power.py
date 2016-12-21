9ad1f83ba993f012ee2f30c458d3446195c4d6ef
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2016-12-21 13:32:03 +0100

    Add logging to power module and fetch, improve network handling

--- lib/brick/power.py
+++ lib/brick/power.py
@@ -1,24 +1,23 @@
 from lib.brick.basic import Basic
-from lib.snips.fetch import get_remote
+from lib.snips.fetch import make_param_req, send_get_req
 
 
 class Power(Basic):
     def __init__(self, *args, url=None, hostname=None, family=None, **kwargs):
         super().__init__(*args, **kwargs)
-        self.url = url
-        self.hostname = hostname
+        self.base_url = url.format(hostname=hostname)
         self.family = family
 
     def do_fire(self, lift=True):
-        rsp, err = get_remote(
-            self.url.format(hostname=self.hostname),
-            power=''.join([self.family, self.prime]),
-            state='full' if lift else 'null',
-        )
-        if err:
-            self.message('full power error', err, lvl='error')
-            return False
-        return rsp and rsp.status == 200
+        with send_get_req(make_param_req(
+                self.base_url,
+                power=''.join([self.family, self.prime]),
+                state='full' if lift else 'null',
+        )) as rsp:
+            return rsp and rsp.status == 200
+
+        self.log.error('connection error [{}]', self.base_url)
+        return False
 
     def full(self):
         return self.do_fire(lift=True)
--- lib/snips/fetch.py
+++ lib/snips/fetch.py
@@ -1,16 +1,26 @@
-from contextlib import closing
+from contextlib import closing, contextmanager
 from urllib.error import URLError
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
+from lib.snips.alert import Log
 
-def get_remote(url, **data):
-    request = Request('{}/?{}'.format(
-        url.rstrip('/'), urlencode(data)
-    ))
+LOG = Log.get(__name__)
 
+
+def make_param_req(url, **data):
+    return Request('{}/?{}'.format(url.rstrip('/'), urlencode(data)))
+
+
+def make_paths_req(url, *parts):
+    return Request('{}/{}'.format(url.rstrip('/'), '/'.join(parts)))
+
+
+@contextmanager
+def send_get_req(request):
     try:
         with closing(urlopen(request)) as response:
-            return response, None
+            yield response
     except URLError as ex:
-        return None, ex
+        LOG.error('{r.type} connection to {r.host} failed: {}', ex, r=request)
+        yield None
